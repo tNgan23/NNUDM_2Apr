df9adc1929b8e0b6c17ff121c9f132e45fa7eada
Ngantl <huynhthitrucngantl@gmail.com>
2024-04-02 21:45:04 +0700

    init

---
+++ configs/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  SECRET_KEY: "TUNG123",
+  EXPIRE_JWT: "1d",
+  hostName: "localhost:3000",
+  Host: "sandbox.smtp.mailtrap.io",
+  Port: " 25",
+  Username: "ac79cf3e3ce28d",
+  Password: "63a09d652bb3bf",
+};
